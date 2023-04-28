a94568260b9a50f9c288496616e599a375bd0233
mcbaiyun <61493882+mcbaiyun@users.noreply.github.com>
2023-04-28 18:03:51 +0800

    add loginpage

---
+++ src/com/team5/websocket/FileReader.java
@@ -0,0 +1,28 @@
+package com.team5.websocket;
+
+import java.io.*;
+
+public class FileReader {
+    public FileReader() {
+    }
+
+    public static String readFile(String fileName) {
+        String result="";
+        File file = new File(fileName);
+        Reader reader = null;
+        try {
+            reader = new InputStreamReader(new FileInputStream(file));
+            int tempchar;
+            while ((tempchar = reader.read()) != -1) {
+                if (((char) tempchar) != '\r') {
+                    result+=(char) tempchar;
+                    //System.out.print((char) tempchar);
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
--- src/com/team5/websocket/Manager.java
+++ src/com/team5/websocket/Manager.java
@@ -5,14 +5,36 @@ public class Manager {
     private Response response=new Response();
     public static final String br ="\r\n";
     public Response getResponse() {
-        if(!request.method.equals("GET")){
+        if(!request.method.equals("GET")&&!request.method.equals("POST")){
+            System.out.println("checked method which not allowed:"+request.method);
             response.setData("<html><body><h1>Methon not allowed!</h1></body></html>");
             response.httpStatus="403 AccessDenied";
             return this.response;
-
         }
-        defReturn(response);
+        System.out.println("request:"+request.data);
+
+        if(request.data.equals("/")){
+            toLogin(response);
+        }
+
+        if(request.data.equals("/login")) {
+            getLogin(response);
+        }
+
+        else {
+            defReturn(response);
+        }
+
         return this.response;
+    }
+    private void getLogin(Response response){
+        FileReader reader = new FileReader();
+        response.setData(reader.readFile("src/websites/login.html"));
+    }
+    private void toLogin(Response response){
+        response.httpStatus="302 moved";
+        response.addHearder("Location: \\login");
+        //response.setData("<html><body><h1>Hello, world!</h1></body></html>");
     }
     private void defReturn(Response response){
         response.setData("<html><body><h1>Hello, world!</h1></body></html>");
